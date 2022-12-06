20b66fe4461b02474ef6ec2f0d08aa59bc96597e
Mateusz Mueller <meeetju@gmail.com>
2022-12-06 22:48:57 +0100

    day 5 part 2

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -16,23 +16,30 @@ fn main() {
                         stacks_section = false;
                         continue;
                     }
-                    println!("Exctracting initial crates on stacks");
                     for (i, _) in value.chars().enumerate().step_by(4) {
                         let stack_crate = &value[i+1..i+2].to_string().clone();
                         let stack_index = i/4;
                         if stack_crate != " " {
-                            println!("Put {} at {}", stack_crate, stack_index);
                             stacks[stack_index].insert(0, stack_crate.clone());
                         }
                     }
                 } else {
                     if !value.is_empty() {
-                        println!("Exctracting moves");
                         let numbers: Vec<u32> = value.split(" ").filter(|value| value.parse::<u32>().is_ok()).map(|value| value.parse::<u32>().unwrap()).collect();
+
+                        // one crate at a time crane-9000
+                        // for _ in 0..numbers[0] {
+                        //     let moved_crate = stacks[numbers[1] as usize - 1].pop().unwrap();
+                        //     stacks[numbers[2] as usize - 1].push(moved_crate);
+                        // }
+
+                        // multiple crates at a time crane-9001
+                        let mut moved_crates: Vec<String> = vec![];
                         for _ in 0..numbers[0] {
-                            let moved_crate = stacks[numbers[1] as usize - 1].pop().unwrap();
-                            stacks[numbers[2] as usize - 1].push(moved_crate);
+                            moved_crates.push(stacks[numbers[1] as usize - 1].pop().unwrap());
                         }
+                        moved_crates.reverse();
+                        stacks[numbers[2] as usize - 1].append(&mut moved_crates);
                     }
                 }
             }
